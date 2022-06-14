1afe3deb74b86bce3125293def1c98eea63f12a0
matiasariass <105993400+matiasariass@users.noreply.github.com>
2022-06-14 15:40:49 -0300

    Update simCreditoVol2.js

--- simCreditoVol2.js
+++ simCreditoVol2.js
@@ -1,84 +1,119 @@
-function credito() {
+const nameIngresado = document.querySelector("#nombreInput");
+const paisIngresado = document.querySelector("#paisInput");
+const montoIngresado = document.querySelector("#montoInput");
+const cuotasIngresadas = document.querySelector("#cuotasInput");
+
+const nameError = document.querySelector("#nombreError");
+const paisError = document.querySelector("#paisError");
+const montoError = document.querySelector("#montoError");
+const cuotasError = document.querySelector("#cuotasError");
+
+const button = document.querySelector("#boton");
+
+button.addEventListener('click',(event)=>{
+    event.preventDefault();
+    validateEmpty(nameIngresado.value, nameIngresado, nombreError, "Nombre");
+    validateEmpty(paisIngresado.value, paisIngresado, paisError, "País");
+    validateEmpty(montoIngresado.value, montoIngresado, montoError, "Monto");
+    validateEmpty(cuotasIngresadas.value, cuotasIngresadas, cuotasError, "Cuotas");
+});
+
+function validateEmpty(valueInput, divInput, divError, nameInput){
+    if(valueInput.length == 0){
+        showError(divInput, divError, nameInput);
+    }else{
+        hideError(divInput, divError);
+   }
+}
+function showError(divInput, divError, nameInput){
+   divInput.style.border='1px solid red';
+   divError.innerHTML = `<img class="icon-error" src="./icon-error.svg" alt="">
+   <p class="error">${nameInput} no puede estar vacío</p>`; 
+}
+function hideError(divInput, divError){
+    divInput.style.border='1px solid hsl(246,25%,77%)';
+    divError.innerHTML = ``;
+}
+
+
+
+// function credito() {
   
-  let username = prompt("Por favor, ingrese su nombre:")
-  let paisIngresado = capitalizeFirstLetter(prompt('Introduzca su pais')) 
-  let interesBruto = calcularInteres(paisIngresado)
-  let interesConDescuentoAplicado = aplicarDescuento(interesBruto)
+
+//   let paisIngresado = capitalizeFirstLetter(prompt('Introduzca su pais')) 
+//   let interesBruto = calcularInteres(paisIngresado)
+//   let interesConDescuentoAplicado = aplicarDescuento(interesBruto)
   
-  let creditoAceptado = false;
+//   let creditoAceptado = false;
 
-  while (!creditoAceptado) {
+//   while (!creditoAceptado) {
 
-    let monto = parseInt(prompt("Ingrese el monto del credito!"))
-    let montoPermitido = (monto < 2000000);
+//     let monto = parseInt(prompt("Ingrese el monto del credito!"))
+//     let montoPermitido = (monto < 2000000);
 
-    while (!montoPermitido) {
-      monto = parseInt(prompt("Ingreso un monto mayor a 2.000.000, por favor ingrese uno nuevo."))
-      montoPermitido = (monto < 2000000);
-    }
+//     while (!montoPermitido) {
+//       monto = parseInt(prompt("Ingreso un monto mayor a 2.000.000, por favor ingrese uno nuevo."))
+//       montoPermitido = (monto < 2000000);
+//     }
 
-    let meses = parseInt(prompt("Ingrese la cantidad de cuotas a pagar!"))
-    let impuestoTotal = (0.15 + interesConDescuentoAplicado) // 0.15 = impuesto de plataforma
-    let resultadoTotal = (monto * impuestoTotal).toFixed(2)
-    let resultadoPorMes = (resultadoTotal / meses).toFixed(2)
+//     let meses = parseInt(prompt("Ingrese la cantidad de cuotas a pagar!"))
+//     let impuestoTotal = (0.15 + interesConDescuentoAplicado) // 0.15 = impuesto de plataforma
+//     let resultadoTotal = (monto * impuestoTotal).toFixed(2)
+//     let resultadoPorMes = (resultadoTotal / meses).toFixed(2)
 
-    creditoAceptado = confirm(username + (", Usted en total pagara: " + resultadoTotal))
+//     creditoAceptado = confirm(username + (", Usted en total pagara: " + resultadoTotal))
 
-    if (creditoAceptado)
-      alert("¡Credito aceptado! Usted pagara: " + resultadoPorMes + " por mes. ¡Gracias por confiar!");
-  }
-}
+//     if (creditoAceptado)
+//       alert("¡Credito aceptado! Usted pagara: " + resultadoPorMes + " por mes. ¡Gracias por confiar!");
+//   }
+// }
 
-const paises = [{
-  id: 1,
-  nombre: "Argentina",
-  importe: 1.21,
-}, {
-  id: 2,
-  nombre: "Colombia",
-  importe: 1.31,
-}, {
-  id: 3,
-  nombre: "Chile",
-  importe: 1.91,
-}, {
-  id: 4,
-  nombre: "Brasil",
-  importe: 1.41,
-}, {
-  id: 5,
-  nombre: "Venezuela",
-  importe: 1.51,
-}]
+// const paises = [{
+//   id: 1,
+//   nombre: "Argentina",
+//   importe: 1.21,
+// }, {
+//   id: 2,
+//   nombre: "Colombia",
+//   importe: 1.31,
+// }, {
+//   id: 3,
+//   nombre: "Chile",
+//   importe: 1.91,
+// }, {
+//   id: 4,
+//   nombre: "Brasil",
+//   importe: 1.41,
+// }, {
+//   id: 5,
+//   nombre: "Venezuela",
+//   importe: 1.51,
+// }]
 
 
-function calcularInteres(paisIngresado) {
+// function calcularInteres(paisIngresado) {
 
-  let resultadoBusqueda = paises.find(pais => pais.nombre === paisIngresado)
+//   let resultadoBusqueda = paises.find(pais => pais.nombre === paisIngresado)
 
-  if (resultadoBusqueda) {
-    return resultadoBusqueda.importe
-  } else {
-    alert("Solo los siguientes paises estan habilitados: Argentina, Brasil, Chile, Uruguay, Venezuela")
-  }
+//   if (resultadoBusqueda) {
+//     return resultadoBusqueda.importe
+//   } else {
+//     alert("Solo los siguientes paises estan habilitados: Argentina, Brasil, Chile, Uruguay, Venezuela")
+//   }
 
-  // EXTRAER DATOS DE OBJETOS - retorna el id de cada pais
-  let idReales = paises.map(pais => pais.id);
-  console.log(idReales)
+// }
+//  // DESCUENTO DEL 15% SI EL IMPORTE ES MAYOR A 1.50
+// function aplicarDescuento(interesBruto) {
+//   if (interesBruto > 1.50) {
+//     return interesBruto * 0.85; 
+//   } else {
+//     return interesBruto;
+//   }
+// }
+// function capitalizeFirstLetter(paisIngresado) {
+//   return paisIngresado.charAt(0).toUpperCase() + paisIngresado.slice(1);
+// }
 
-}
- // DESCUENTO DEL 15% SI EL IMPORTE ES MAYOR A 1.50
-function aplicarDescuento(interesBruto) {
-  if (interesBruto > 1.50) {
-    return interesBruto * 0.85; 
-  } else {
-    return interesBruto;
-  }
-}
-function capitalizeFirstLetter(paisIngresado) {
-  return paisIngresado.charAt(0).toUpperCase() + paisIngresado.slice(1);
-}
-
-console.log(capitalizeFirstLetter('foo')); 
+// console.log(capitalizeFirstLetter('foo')); 
   
-credito()
+// credito()
