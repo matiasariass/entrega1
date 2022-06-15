b7314b14581ad90b8c84065f43be020763247d3c
matiasariass <105993400+matiasariass@users.noreply.github.com>
2022-06-15 10:27:58 -0300

    Update simCreditoVol2.js

--- simCreditoVol2.js
+++ simCreditoVol2.js
@@ -36,6 +36,18 @@ function hideError(divInput, divError){
 }
 
 
+let popup = document.getElementById ("popup");
+
+function abrirPopup(){
+    popup.classList.add("open-popup");
+
+}
+
+function cerrarPopup(){
+    popup.classList.remove("open-popup");
+    
+}
+
 
 // function credito() {
   
